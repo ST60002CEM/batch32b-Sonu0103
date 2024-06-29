169370ff97df05dd08bbb3efc668bceddc592582
Sonu0103 <sonuhp2002@gmail.com>
2024-06-29 17:06:22 +0545

    changes in code

--- foodie/macos/Flutter/GeneratedPluginRegistrant.swift
+++ foodie/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,7 @@
 import FlutterMacOS
 import Foundation
 
+import connectivity_plus
 import facebook_auth_desktop
 import flutter_secure_storage_macos
 import google_sign_in_ios
@@ -13,6 +14,7 @@ import shared_preferences_foundation
 import sign_in_with_apple
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
   FacebookAuthDesktopPlugin.register(with: registry.registrar(forPlugin: "FacebookAuthDesktopPlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
