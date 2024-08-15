0b857aa17d91a63665990ff6f1838dc45b744d54
Sonu0103 <sonuhp2002@gmail.com>
2024-08-15 14:20:12 +0545

    changes in dashboard screen

---
+++ foodie/lib/features/dashboard/presentation/view/dashboard_view.dart
@@ -0,0 +1,319 @@
+// import 'package:flutter/material.dart';
+//
+// class DashboardView extends StatefulWidget {
+//   const DashboardView({Key? key}) : super(key: key);
+//
+//   @override
+//   State<DashboardView> createState() => _DashboardViewState();
+// }
+//
+// class _DashboardViewState extends State<DashboardView> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         backgroundColor: Colors.teal,
+//         title: const Text(
+//           'Dashboard',
+//           style: TextStyle(
+//             color: Colors.white,
+//             fontWeight: FontWeight.bold,
+//           ),
+//         ),
+//         actions: [
+//           IconButton(
+//             icon: const CircleAvatar(
+//               radius: 20,
+//               backgroundImage: AssetImage('assets/images/profile.jpg'),
+//             ),
+//             onPressed: () {
+//               // Implement profile button functionality
+//             },
+//           ),
+//         ],
+//       ),
+//       backgroundColor: Colors.grey[200],
+//       body: SingleChildScrollView(
+//         child: Column(
+//           crossAxisAlignment: CrossAxisAlignment.stretch,
+//           children: [
+//             const SizedBox(height: 20),
+//             Padding(
+//               padding: const EdgeInsets.symmetric(horizontal: 20.0),
+//               child: ElevatedButton(
+//                 onPressed: () {
+//                   // Implement search functionality
+//                 },
+//                 style: ElevatedButton.styleFrom(
+//                   backgroundColor: Colors.teal,
+//                   foregroundColor: Colors.white,
+//                   shape: RoundedRectangleBorder(
+//                     borderRadius: BorderRadius.circular(10),
+//                   ),
+//                   padding: const EdgeInsets.symmetric(vertical: 15),
+//                 ),
+//                 child: const Row(
+//                   mainAxisAlignment: MainAxisAlignment.center,
+//                   children: [
+//                     Icon(Icons.search),
+//                     SizedBox(width: 10),
+//                     Text('Search'),
+//                   ],
+//                 ),
+//               ),
+//             ),
+//             const SizedBox(height: 20),
+//             Container(
+//               color: Colors.teal[400],
+//               padding: const EdgeInsets.all(20),
+//               child: Row(
+//                 children: [
+//                   Expanded(
+//                     child: Column(
+//                       crossAxisAlignment: CrossAxisAlignment.start,
+//                       children: [
+//                         const Text(
+//                           'The Fastest\nFood Delivery',
+//                           style: TextStyle(
+//                             color: Colors.white,
+//                             fontSize: 26,
+//                             fontWeight: FontWeight.bold,
+//                           ),
+//                         ),
+//                         const SizedBox(height: 20),
+//                         ElevatedButton(
+//                           onPressed: () {
+//                             // Implement order now functionality
+//                           },
+//                           style: ElevatedButton.styleFrom(
+//                             backgroundColor: Colors.white,
+//                             foregroundColor: Colors.teal,
+//                             padding: const EdgeInsets.symmetric(
+//                                 horizontal: 30, vertical: 15),
+//                           ),
+//                           child: const Text('Order Now'),
+//                         ),
+//                       ],
+//                     ),
+//                   ),
+//                   const SizedBox(width: 20),
+//                   ClipRRect(
+//                     borderRadius: BorderRadius.circular(20),
+//                     child: Image.asset(
+//                       'assets/images/logo2.png',
+//                       height: 150,
+//                       width: 150,
+//                       fit: BoxFit.cover,
+//                     ),
+//                   ),
+//                 ],
+//               ),
+//             ),
+//             const SizedBox(height: 20),
+//             Padding(
+//               padding: const EdgeInsets.symmetric(horizontal: 20.0),
+//               child: Column(
+//                 crossAxisAlignment: CrossAxisAlignment.start,
+//                 children: [
+//                   const Text(
+//                     'Categories',
+//                     style: TextStyle(
+//                       fontSize: 22,
+//                       fontWeight: FontWeight.bold,
+//                       color: Colors.teal,
+//                     ),
+//                   ),
+//                   const SizedBox(height: 10),
+//                   Row(
+//                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//                     children: [
+//                       GestureDetector(
+//                         onTap: () {
+//                           Navigator.push(
+//                             context,
+//                             MaterialPageRoute(
+//                               builder: (context) => AllItemsPage(),
+//                             ),
+//                           );
+//                         },
+//                         child: _buildCategoryItem('All', 'assets/images/new.jpg'),
+//                       ),
+//                       _buildCategoryItem('Burger', 'assets/images/burger.jpg'),
+//                       _buildCategoryItem('Pizza', 'assets/images/pizza.jpg'),
+//                       _buildCategoryItem('Dessert', 'assets/images/dessert.jpg'),
+//                     ],
+//                   ),
+//                   const SizedBox(height: 20),
+//                   const Text(
+//                     'Popular',
+//                     style: TextStyle(
+//                       fontSize: 22,
+//                       fontWeight: FontWeight.bold,
+//                       color: Colors.teal,
+//                     ),
+//                   ),
+//                   const SizedBox(height: 10),
+//                   Row(
+//                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//                     children: [
+//                       _buildPopularItem(
+//                           'Burger', 'Rs.200', 'assets/images/burger.jpg'),
+//                       _buildPopularItem(
+//                           'Chicken Pizza', 'Rs.300', 'assets/images/pizza.jpg'),
+//                     ],
+//                   ),
+//                   const SizedBox(height: 20),
+//                   const Text(
+//                     'Cuisine',
+//                     style: TextStyle(
+//                       fontSize: 22,
+//                       fontWeight: FontWeight.bold,
+//                       color: Colors.teal,
+//                     ),
+//                   ),
+//                   const SizedBox(height: 10),
+//                   Row(
+//                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//                     children: [
+//                       _buildCategoryItem(
+//                           'Chinese', 'assets/images/chinese.jpg'),
+//                       _buildCategoryItem('Indian', 'assets/images/indian.jpg'),
+//                     ],
+//                   ),
+//                 ],
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//       bottomNavigationBar: BottomNavigationBar(
+//         selectedItemColor: Colors.teal,
+//         unselectedItemColor: Colors.grey,
+//         items: const [
+//           BottomNavigationBarItem(
+//             icon: Icon(Icons.home),
+//             label: 'Home',
+//           ),
+//           BottomNavigationBarItem(
+//             icon: Icon(Icons.search),
+//             label: 'Search',
+//           ),
+//           BottomNavigationBarItem(
+//             icon: Icon(Icons.shopping_cart),
+//             label: 'Cart',
+//           ),
+//           BottomNavigationBarItem(
+//             icon: Icon(Icons.person),
+//             label: 'Profile',
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+//
+//   Widget _buildCategoryItem(String label, String imagePath) {
+//     return Column(
+//       children: [
+//         ClipRRect(
+//           borderRadius: BorderRadius.circular(10),
+//           child: Image.asset(
+//             imagePath,
+//             height: 80,
+//             width: 80,
+//             fit: BoxFit.cover,
+//           ),
+//         ),
+//         const SizedBox(height: 5),
+//         Text(
+//           label,
+//           style: const TextStyle(
+//             fontWeight: FontWeight.bold,
+//             color: Colors.black87,
+//           ),
+//         ),
+//       ],
+//     );
+//   }
+//
+//   Widget _buildPopularItem(String label, String price, String imagePath) {
+//     return Column(
+//       children: [
+//         ClipRRect(
+//           borderRadius: BorderRadius.circular(10),
+//           child: Image.asset(
+//             imagePath,
+//             height: 100,
+//             width: 150,
+//             fit: BoxFit.cover,
+//           ),
+//         ),
+//         const SizedBox(height: 5),
+//         Text(
+//           label,
+//           style: const TextStyle(
+//               fontSize: 16, fontWeight: FontWeight.bold, color: Colors.black87),
+//         ),
+//         Text(
+//           'Price: $price',
+//           style: const TextStyle(
+//               fontSize: 16, fontWeight: FontWeight.bold, color: Colors.black54),
+//         ),
+//       ],
+//     );
+//   }
+// }
+//
+// class AllItemsPage extends StatelessWidget {
+//   AllItemsPage({Key? key}) : super(key: key);
+//
+//   final ScrollController _scrollController = ScrollController();
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         backgroundColor: Colors.teal,
+//         title: const Text('All Items'),
+//         leading: IconButton(
+//           icon: const Icon(Icons.arrow_back),
+//           onPressed: () {
+//             Navigator.pop(context);
+//           },
+//         ),
+//         actions: [
+//           IconButton(
+//             icon: const Icon(Icons.refresh),
+//             onPressed: () {
+//               _scrollController.animateTo(
+//                 0.0,
+//                 duration: const Duration(milliseconds: 500),
+//                 curve: Curves.easeInOut,
+//               );
+//             },
+//           ),
+//         ],
+//       ),
+//       body: ListView.builder(
+//         controller: _scrollController,
+//         itemCount: 20,
+//         itemBuilder: (context, index) {
+//           return ListTile(
+//             leading: ClipRRect(
+//               borderRadius: BorderRadius.circular(10),
+//               child: Image.asset(
+//                 'assets/images/burger.jpg',
+//                 height: 50,
+//                 width: 50,
+//                 fit: BoxFit.cover,
+//               ),
+//             ),
+//             title: Text('Burger ${index + 1}'),
+//             subtitle: Text('Description of item ${index + 1}'),
+//
+//           );
+//
+//         },
+//       ),
+//     );
+//   }
+// }
