dc51740a130fd05ac6faf1a2f8f44d540c4bdcd9
Sonu0103 <sonuhp2002@gmail.com>
2024-08-17 20:30:22 +0545

    changes in code

--- foodie/macos/Flutter/GeneratedPluginRegistrant.swift
+++ foodie/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,6 +9,7 @@ import connectivity_plus
 import facebook_auth_desktop
 import flutter_secure_storage_macos
 import google_sign_in_ios
+import local_auth_darwin
 import path_provider_foundation
 import shared_preferences_foundation
 import sign_in_with_apple
@@ -18,6 +19,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FacebookAuthDesktopPlugin.register(with: registry.registrar(forPlugin: "FacebookAuthDesktopPlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
+  FLALocalAuthPlugin.register(with: registry.registrar(forPlugin: "FLALocalAuthPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SignInWithApplePlugin.register(with: registry.registrar(forPlugin: "SignInWithApplePlugin"))
