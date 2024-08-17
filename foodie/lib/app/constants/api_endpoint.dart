f91d00143436f6b4afda2f376fdf2e594c3665fb
Sonu0103 <sonuhp2002@gmail.com>
2024-08-17 20:31:52 +0545

    changes in end points

--- foodie/lib/app/constants/api_endpoint.dart
+++ foodie/lib/app/constants/api_endpoint.dart
@@ -3,13 +3,15 @@ class ApiEndpoints {
 
   static const Duration connectionTimeout = Duration(seconds: 1000);
   static const Duration receiveTimeout = Duration(seconds: 1000);
-  static const String baseUrl = "http://192.168.137.1:4000/api/";
+  static const String baseUrl = "http://192.168.1.66:4000/api/";
   //static const String baseUrl = "http://192.168.4.4:3000/api/v1/";
 
   // ====================== Auth Routes ======================
   static const String login = "${baseUrl}user/login";
   static const String register = "${baseUrl}user/register";
   static const String updateUserProfile = "${baseUrl}user/profile";
+  static const String forgotPassword = "${baseUrl}user/forgot-password"; // Added this
+  static const String verifyOtp = "${baseUrl}user/verify-otp"; // Added this
 
   // ====================== Food Routes ======================
   static const String addFood = "${baseUrl}food/add";
