4dfc602b8ae60ccee3a33f0ace49d2653687ee66
Sonu0103 <sonuhp2002@gmail.com>
2024-08-17 20:32:14 +0545

    changes in add to cart

--- foodie/lib/features/home/presentation/view/screen/Detail/Widget/add_to_cart.dart
+++ foodie/lib/features/home/presentation/view/screen/Detail/Widget/add_to_cart.dart
@@ -1,8 +1,8 @@
 import 'package:finalproject/features/home/data/model/product_model.dart';
+import 'package:finalproject/features/home/data/provider/cart_provider.dart';
 import 'package:finalproject/features/home/presentation/view/constants.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:hive/hive.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class AddToCart extends StatefulWidget {
   final Product product;
@@ -18,49 +18,55 @@ class _AddToCartState extends State<AddToCart> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(
-          horizontal: 15,
-      ),
-      child:  Container(
+      padding: const EdgeInsets.symmetric(horizontal: 15),
+      child: Container(
         height: 85,
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(50),
           color: Colors.black,
         ),
-        padding: const EdgeInsets.symmetric(
-          horizontal: 15,
-        ),
+        padding: const EdgeInsets.symmetric(horizontal: 15),
         alignment: Alignment.center,
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             Container(
-                height: 40,
+              height: 40,
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(20),
-                border: Border.all(color: Colors.white,width: 2),
+                border: Border.all(color: Colors.white, width: 2),
               ),
               child: Row(
                 children: [
                   IconButton(
-                      onPressed: () {},
+                    onPressed: () {
+                      if (currentIndex != 1) {
+                        setState(() {
+                          currentIndex--;
+                        });
+                      }
+                    },
                     iconSize: 18,
-                      icon: const Icon(
-                        Icons.remove,
-                        color: Colors.white,
-                      ),
+                    icon: const Icon(
+                      Icons.remove,
+                      color: Colors.white,
+                    ),
                   ),
                   const SizedBox(width: 5),
                   Text(
                     currentIndex.toString(),
                     style: const TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
                     ),
                   ),
                   const SizedBox(width: 5),
                   IconButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      setState(() {
+                        currentIndex++;
+                      });
+                    },
                     iconSize: 18,
                     icon: const Icon(
                       Icons.add,
@@ -70,28 +76,49 @@ class _AddToCartState extends State<AddToCart> {
                 ],
               ),
             ),
-            GestureDetector(
-              onTap: (){
+            Consumer(
+              builder: (context, ref, child) {
+                return GestureDetector(
+                  onTap: () {
+                    final provider = ref.read(cartProvider);
 
-              },
-              child: Container(
-                height: 55,
-                decoration: BoxDecoration(
-                  color: kprimaryColor,
-                  borderRadius: BorderRadius.circular(50),
-                ),
-                alignment: Alignment.center,
-                padding: const EdgeInsets.symmetric(horizontal: 50),
-                child: const Text(
-                  "Add To Cart",
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
+                    widget.product.quantity = currentIndex;
+
+                    provider.toggleFavorite(widget.product);
+
+                    const snackBar = SnackBar(
+                      content: Text(
+                        "Successfully Added!",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 25,
+                          color: Colors.white,
+                        ),
+                      ),
+                      duration: Duration(seconds: 1),
+                    );
+                    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                  },
+                  child: Container(
+                    height: 55,
+                    decoration: BoxDecoration(
+                      color: kprimaryColor,
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                    alignment: Alignment.center,
+                    padding: const EdgeInsets.symmetric(horizontal: 50),
+                    child: const Text(
+                      "Add To Cart",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                    ),
                   ),
-                ),
-              ),
-            )
+                );
+              },
+            ),
           ],
         ),
       ),
